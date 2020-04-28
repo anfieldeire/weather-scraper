a3f8b9b66e454454fde5f1687bc8638f70832db3
anfieldeire <gareth.hasson@gmail.com>
2020-04-28 19:58:54 -0400

    Update zipcode_checker.py

--- zipcode_checker.py
+++ zipcode_checker.py
@@ -16,38 +16,28 @@ def zipcode_checker():
     zip_code = input("Please enter your 5 digit zipcode: ")
 
     if len(zip_code) == 5 and zip_code.isdigit() and re.search("^[0-9]{5}", zip_code):
-        # if re.search("^[0-9]{5}", zip_code):
-            print("Valid ZIP code")
             user_timeframe = input("Please enter the weather timeframe. Options are:"
                                    " today, hourbyhour, tenday or weekend: ")
             if user_timeframe in timeframe:
-
                 url_to_scrape = base_url + '/' + user_timeframe + '/l/' + zip_code
                 weather = requests.get(url_to_scrape).text
                 return_code = requests.get(url_to_scrape)
                 weather_soup = BeautifulSoup(weather, 'lxml')
 
                 if return_code.status_code != 200:
-                    print(return_code.status_code)
                     print("Error: The site is down or there are no matches for the zip code provided\n"
                           "Please try another zip code or try again later")
                     return {'status': 'False'}
                 else:
-                    print(return_code.status_code)
                     result_dict['status'] = 'True'
                     result_dict['timeframe'] = user_timeframe
                     result_dict['soup'] = weather_soup
                     return result_dict
 
             else:
-                print("Timeframe not valid")
+                print("Timeframe not valid. The timeframe must be: today, hourbyhour, tenday or weekend")
                 return {'status': 'False'}
 
     else:
-        print("The input was not valid, please enter a 5 digit zipcode: ")
+        print("The input was not valid, please enter a valid 5 digit zipcode: ")
         return {'status': 'False'}
-
-
-
-
-
